6d813ee7d8bb9f3b85bdd3ae53133903a53c4588
Gabe Hargrave <gabe.hargrave@vacasa.com>
2019-03-09 20:33:01 -0800

    Install and run Prettier

--- src/04-DataMunging/01-Football.js
+++ src/04-DataMunging/01-Football.js
@@ -6,41 +6,39 @@ each team in that season (so Arsenal scored 79 goals against opponents, and had 
 
 Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 */
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('fs')
+const readline = require('readline')
 
-const { findMinSpread, parseLine } = require('./helpers');
+const { findMinSpread, parseLine } = require('./helpers')
 
-
-const FIRST_DATA_LINE = 1;
-const FILE = './src/04-DataMunging/football.dat';
+const FIRST_DATA_LINE = 1
+const FILE = './src/04-DataMunging/football.dat'
 const KEYS = {
   TeamName: 1,
   PointsScored: 6,
   PointsAllowed: 7,
 }
 
-
-let linesParsed = 0;
-let data = [];
+let linesParsed = 0
+let data = []
 
 const rl = readline.createInterface({
   input: fs.createReadStream(FILE),
   crlfDelay: Infinity,
-});
+})
 
-rl.on('line', (line) => {
+rl.on('line', line => {
   if (linesParsed++ < FIRST_DATA_LINE) {
-    return;
+    return
   }
 
-  const parsed = parseLine(line);
+  const parsed = parseLine(line)
   if (parsed.length) {
-    data.push(parsed);
+    data.push(parsed)
   }
-});
+})
 
 rl.on('close', () => {
-  const min = findMinSpread(data, KEYS.PointsScored, KEYS.PointsAllowed);
+  const min = findMinSpread(data, KEYS.PointsScored, KEYS.PointsAllowed)
   console.log({ team: min[KEYS.TeamName] })
 })
