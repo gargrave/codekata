c770f99ee3e92e638f4e920c458e36ce8865031b
Gabe Hargrave <gabe.hargrave@vacasa.com>
2019-03-10 12:44:58 -0700

    Add README and additional tests

--- src/09-Checkout/checkout.spec.js
+++ src/09-Checkout/checkout.spec.js
@@ -9,18 +9,43 @@ function price(goods) {
   return co.total
 }
 
-assert.equal(0, price(''))
-assert.equal(50, price('A'))
-assert.equal(80, price('AB'))
-assert.equal(115, price('CDBA'))
+function testTotals() {
+  assert.equal(0, price(''))
+  assert.equal(50, price('A'))
+  assert.equal(80, price('AB'))
+  assert.equal(115, price('CDBA'))
 
-assert.equal(100, price('AA'))
-assert.equal(130, price('AAA'))
-assert.equal(180, price('AAAA'))
-assert.equal(230, price('AAAAA'))
-assert.equal(260, price('AAAAAA'))
+  assert.equal(100, price('AA'))
+  assert.equal(130, price('AAA'))
+  assert.equal(180, price('AAAA'))
+  assert.equal(230, price('AAAAA'))
+  assert.equal(260, price('AAAAAA'))
 
-assert.equal(160, price('AAAB'))
-assert.equal(175, price('AAABB'))
-assert.equal(190, price('AAABBD'))
-assert.equal(190, price('DABABA'))
+  assert.equal(160, price('AAAB'))
+  assert.equal(175, price('AAABB'))
+  assert.equal(190, price('AAABBD'))
+  assert.equal(190, price('DABABA'))
+}
+
+function testIncremental() {
+  const co = new Checkout(defaultPricingRules)
+  assert.equal(0, co.total)
+
+  co.scan('A')
+  assert.equal(50, co.total)
+
+  co.scan('B')
+  assert.equal(80, co.total)
+
+  co.scan('A')
+  assert.equal(130, co.total)
+
+  co.scan('A')
+  assert.equal(160, co.total)
+
+  co.scan('B')
+  assert.equal(175, co.total)
+}
+
+testTotals()
+testIncremental()
